d3cd33d42e22ace02fd8f7eb3a7e94e3cf55862e
H P <H P@LAPTOP-KASON>
2026-09-06 15:32:02 +0700

    update

--- src/main/java/com/mycom/springbootdemo1/component/schedule/CronSchedule.java
+++ src/main/java/com/mycom/springbootdemo1/component/schedule/CronSchedule.java
@@ -3,21 +3,24 @@ package com.mycom.springbootdemo1.component.schedule;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class CronSchedule {
 
 	@Scheduled(cron = "0/5 * * * * *")
 	public void run() {
-		System.out.println("CronSchedule is running...");
+		log.info("CronSchedule is running...");
 	}
 
 	@Scheduled(cron = "* * 0 * * MON-FRI")
 	public void run2() {
-		System.out.println("CronSchedule is running... at 0.00 every day between monday and friday");
+		log.info("CronSchedule is running... at 0.00 every day between monday and friday");
 	}
 
 	@Scheduled(cron = "* * 0 * * MON,WED,FRI")
 	public void run3() {
-		System.out.println("CronSchedule is running... at 0.00 at monday, wednesday, friday");
+		log.info("CronSchedule is running... at 0.00 at monday, wednesday, friday");
 	}
 }
--- src/main/java/com/mycom/springbootdemo1/component/schedule/FixSchedule.java
+++ src/main/java/com/mycom/springbootdemo1/component/schedule/FixSchedule.java
@@ -5,18 +5,21 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class FixSchedule {
 
 	@Scheduled(fixedDelay = 3, initialDelay = 1, timeUnit = TimeUnit.SECONDS)
 	public void runFixedDelay() {
 		//งานเสร็จ → รอ 3 วินาที → รอบใหม่
-		System.out.println("FixedDelaySchedule is running...");
+		log.info("FixedDelaySchedule is running...");
 	}
 
 	@Scheduled(fixedRate = 3, initialDelay = 1, timeUnit = TimeUnit.SECONDS)
 	public void runFixedRate() {
 		//พยายามให้แต่ละรอบเริ่มห่างกัน 3 วินาที แต่ก็ต้องรอให้งานเสร็จ
-		System.out.println("FixedRateSchedule is running...");
+		log.info("FixedRateSchedule is running...");
 	}
 }
